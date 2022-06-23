888e30ed0f12e878288c9f2324d0e88fd12f7637
Milena <milenatukiendorf@gmail.com>
2022-06-23 19:14:54 +0200

    Show popup

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,24 @@
-const showPopupButton= document.getElementById("show-popup-button");
-console.log("showPopupButton", showPopupButton);
+console.log("Main start");
 
-const onPopupButtonClick = () => {
-    alert('Popup works');
+popup = document.getElementById('popup');
+showPopupButton = document.getElementById('show-popup-button');
+
+console.log('showPopupButton', showPopupButton);
+console.log('popup', popup);
+
+const openPopup = () => {
+    popup  = document.getElementById('popup');
+    console.log('togglePopup popup', popup);
+
+    popup.classList.add("show-popup");
 }
 
+const closePopup = () => {
+    popup = document.getElementById('popup');
+    console.log('closePopup popup', popup);
 
-showPopupButton.addEventListener('click', onPopupButtonClick );
+    popup.classList.remove("show-popup");
+}
+
+showPopupButton.addEventListener('click', openPopup);
+popup.addEventListener('click', closePopup);
